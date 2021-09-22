d084dbb851773109e522215ff29465af3ce7f103
niubrandon <niubrandon@gmail.com>
2021-09-22 13:37:47 -0400

    created a client.js

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host:"135.23.223.133",
+    port: 50542
+  });
+
+  //interpret buffer as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", (data) => {
+    // code that does something when the connection is first established
+    console.log(`data is ${data}`);
+  });
+
+  return conn;
+};
+
+console.log("Connecting...");
+//connect();
+
+module.exports = {connect};
+
